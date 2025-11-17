86725abb08cd187a87ef4139d2c049dca4dbd25b
kokoyasa <krms0727@gmail.com>
2025-11-17 23:44:00 +0900

    進捗
    以下のテストに成功 (17/25)
    ｛初期化、先頭にデータを追加、末尾にデータを追加、pのノードを削除｝
    以下のテストに失敗 (8/25)
    ｛pの次のノードを削除、pの前のノードを削除｝

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -21,45 +21,65 @@ void initialize_list(list* l)
 
 
 // 先頭にデータを追加
-void push_front(list* l, node* p)
-{
+void push_front(list* l, node* p){
+	if (!l || !p) return; //リストとノードが存在しなければ終了する。
+	p->pPrev = NULL; //先頭であるから初期化(＝前のノードは存在しないことになる。)
+	p->pNext = l->header; //元の先頭を'p'の次に。
+	
+	if (l->header) l->header->pPrev = p; //リストにノードがある場合、先頭のノードを'p'の後ろに。
+	else l->footer = p; //リストが空ならfooterも'p'に。
+	l->header = p; //リストの先頭を'p'に。
 }
 
 // 末尾にデータを追加
-void push_back(list* l, node* p)
-{
+void push_back(list* l, node* p){
+	if (!l || !p) return; //同様に存在しなければ終了。
+	p->pNext = NULL; //末尾に挿入され、続きはないのでNULLにする。
+	p->pPrev = l->footer; //元の末尾を前に。
+
+	if (l->footer) l->footer->pNext = p; //リストにノードがあるなら、末尾のノードの次を'p'に。
+	else l->header = p; //リストが空ならheaderも'p'に。
+	l->footer = p; //リストの末尾を'p'に。
 }
 
 // pのノードを削除
-void remove_node(list* l, node* p)
-{
+void remove_node(list* l, node* p){
+	if (!l || !p) return; //同様に(以下略)
+	// pの前にノードがある場合、その次を'p'の次へ繋ぎ直す(pをリストから除外)。
+	if (p->pPrev) p->pPrev->pNext = p->pNext;
+	else l->header = p->pNext; //(p->pPrev == NULL)ならheaderを'p'の次に。
+
+	// pの次にノードがある場合、その前を'p'の前に繋ぐ。
+	if (p->pNext) p->pNext->pPrev = p->pPrev;
+	else l->footer = p->pPrev; //(p->pNext == NULL)footerを一つ前に。
+	
+	p->pNext = NULL; //ポインタを完全に外す。
+	p->pPrev = NULL;
 }
 
 // pの次のノードを削除
-void remove_next(list* l, node* p)
-{
+void remove_next(list* l, node* p){
+
+
+
 }
 
 // pの前のノードを削除
-void remove_prev(list* l, node* p)
-{
+void remove_prev(list* l, node* p){
+
+
+
 }
 
 
 // pの次のノードを取得
-node* get_next(list* l, node* p)
-{
+node* get_next(list* l, node* p){
 	if (!p) return NULL;
 	return p->pNext;
-
-	return NULL;
 }
 
 // pの前のノードを取得
-node* get_prev(list* l, node* p)
-{
+node* get_prev(list* l, node* p){
 	if (!p) return NULL;
 	return p->pPrev;
-
-	return NULL;
 }
